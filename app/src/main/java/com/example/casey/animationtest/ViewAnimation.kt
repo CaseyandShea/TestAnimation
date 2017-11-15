1f153b3e766c138b72f97bd4956da52a771fc858
Casey <casey@flashexpress.com>
2017-11-15 22:05:56 +0800

    增加是否重复等属性

--- app/src/main/java/com/example/casey/animationtest/ViewAnimation.kt
+++ app/src/main/java/com/example/casey/animationtest/ViewAnimation.kt
@@ -3,6 +3,7 @@ package com.example.casey.animationtest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import kotlinx.android.synthetic.main.activity_view_animation.*
 
 class ViewAnimation : AppCompatActivity() {
@@ -14,6 +15,8 @@ class ViewAnimation : AppCompatActivity() {
             val alphaAnimation = AlphaAnimation(0f, 1f)
             alphaAnimation.duration = 2000//动画持续时间
             alphaAnimation.fillAfter = true
+            alphaAnimation.repeatMode = Animation.REVERSE //重复的模式 Animation.RESTART：重新开始一次   Animation.REVERSE：反转
+            alphaAnimation.repeatCount = 12//重复次数
             happy.startAnimation(alphaAnimation)
         }
     }
