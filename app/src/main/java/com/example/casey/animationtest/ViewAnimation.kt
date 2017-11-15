40ad7562622381f3ffc0a3bd75c61d0aedbe74e8
Casey <casey@flashexpress.com>
2017-11-15 21:59:54 +0800

    透明度动画

--- app/src/main/java/com/example/casey/animationtest/ViewAnimation.kt
+++ app/src/main/java/com/example/casey/animationtest/ViewAnimation.kt
@@ -1,12 +1,20 @@
 package com.example.casey.animationtest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AlphaAnimation
+import kotlinx.android.synthetic.main.activity_view_animation.*
 
 class ViewAnimation : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_animation)
+        alpha.setOnClickListener {
+            val alphaAnimation = AlphaAnimation(0f, 1f)
+            alphaAnimation.duration = 2000//动画持续时间
+            alphaAnimation.fillAfter = true
+            happy.startAnimation(alphaAnimation)
+        }
     }
 }
